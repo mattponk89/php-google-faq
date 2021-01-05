39787730375d2c5e3ff018de8a883edc2632e6db
Matteo Ponchietti <matteoponchietti@gmail.com>
2021-01-05 17:04:04 +0000

    Header OK

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="img/google-icon.svg">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Domande frequenti – Privacy e termini – Google</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row d-flex flex-jc-space-b flex-align-center">
+                <div class="col">
+                    <div class="logo d-flex flex-align-center">
+                        <img src="img/google-logo.svg" alt="logo google">
+                        <a href="#">Privacy e Termini</a>
+                    </div>
+                </div>
+                <div class="col d-flex flex-align-center">
+                    <a href="#"><i class="fa fa-th" aria-hidden="true"></i></a>
+                    <div class="avatar_user">
+                        <a href="#"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col menu">
+                    <ul>
+                        <li>
+                            <a href="#">Introduzione</a>
+                        </li>
+                        <li>
+                            <a href="#">Norme sulla privacy</a>
+                        </li>
+                        <li>
+                            <a href="#">Termini di servizio</a>
+                        </li>
+                        <li>
+                            <a href="#">Tecnologie</a>
+                        </li>
+                        <li>
+                            <a class="item_active" href="#">Domande Frequenti</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
